0702a78825524f8fae4a1c13073947452e80faa8
Abuarab <aabuarab@88e9fe679793.ant.amazon.com>
2020-01-14 16:14:33 +0200

    Add more currencies to cover number of decimal digits after zero

--- addons/payfort_fort/lib/payfortFort/classes/Helper.php
+++ addons/payfort_fort/lib/payfortFort/classes/Helper.php
@@ -98,7 +98,7 @@ class Payfort_Fort_Helper
             'BHD' => 3,
             'LYD' => 3,
             'IQD' => 3,
-            'CLF' => 4,
+            'CLF' => 0,
             'BIF' => 0,
             'DJF' => 0,
             'GNF' => 0, 
@@ -113,6 +113,7 @@ class Payfort_Fort_Helper
             'VND' => 0,
             'VUV' => 0,
             'XAF' => 0,
+            'BYR' => 0,
         );
         if (isset($arrCurrencies[$currency])) {
             $decimalPoint = $arrCurrencies[$currency];
